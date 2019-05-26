bcd3bb4b6135a9a76a8fd5f9d19942394a99e0ab
DexiGG <invokedex@gmail.com>
2019-05-27 00:50:38 +0600

    git commi -m WpfHM-Student

---
+++ WpfHM-Student/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfHM_Student
+{
+    public class Student
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string FullName { get; set; }
+        public string GroupName { get; set; }
+    }
+}
